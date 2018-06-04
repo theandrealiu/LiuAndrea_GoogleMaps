33bf8987661842f2eff29cb880000ef265fbe7b8
Andrea <theandrealiu@gmail.com>
2018-06-04 09:14:47 -0700

    newer

--- app/src/main/java/com/example/liua4835/mymapsproject/MapsActivity.java
+++ app/src/main/java/com/example/liua4835/mymapsproject/MapsActivity.java
@@ -1,7 +1,18 @@
 package com.example.liua4835.mymapsproject;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +21,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private EditText locationSearch = ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +55,98 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        // Add a marker at your place of birth and move the camera to it
+        // When the marker is tapped, display "Born here"
+        LatLng birth = new LatLng(32.7157, -117.1611);
+        mMap.addMarker(new MarkerOptions().position(birth).title("Born here"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(birth));
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d("MyMapsApp", "Failed FINE Permission Check");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 2);
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d("MyMapsApp", "Failed COARSE Permission Check");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 2);
+        }
+
+        if ((ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) ||
+                (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)) {
+            mMap.setMyLocationEnabled(true);
+        }
+        locationSearch = (EditText) findViewById(R.id.editText_addr);
+    }
+
+
+
+    public void onSearch(View v){
+        String location = locationSearch.getText().toString();
+
+        List<Address> addressList = null;
+        List <Address> addressListzip = null;
+
+        LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = service.getBestProvider(criteria,false);
+
+        Log.d("MyMapsApp","onSearch: location = " + location);
+        Log.d("MyMapsApp","onSearch: provider " + provider);
+
+        LatLng userLocation = null;
+
+        try{
+            if(locationManager != null){
+                Log.d("MyMapsApp","onSearch: LocationManager is not null");
+
+                if((myLocation = locationManager.getLasKnownLocation(LocationManager.NETWORK_PROVIDER)) != null) {
+                    userLocation = new LatLng(myLocation.getLatitude(), myLocation.getLongitude);
+                    Log.d("MyMapApp","onSearch: Using NETWORK_PROVIDER userLocation is: " +myLocation.getLatitude() + " " + myLocation.getLongitude());
+                    Toast.makeText(this, "UserLoc" + myLocation.getLatitude() + " " + myLocation.getLongitude()).show();
+                } else if((myLocation = locationManager.getLasKnownLocation(LocationManager.GPS_PROVIDER))!= null){
+                    userLocation = new LatLng(myLocation.getLatitude(), myLocation.getLongitude);
+                    Log.d("MyMapApp","onSearch: Using NETWORK_PROVIDER userLocation is: " +myLocation.getLatitude() + " " + myLocation.getLongitude());
+                    Toast.makeText(this, "UserLoc" + myLocation.getLatitude() + " " + myLocation.getLongitude();
+                }
+                else{
+                    Log.d("MyMapsApp","onSearch: myLocation is null from getLastKnownLocation");
+                }
+            }
+        }
+        catch(SecurityException|IllegalArgumentException e){
+            Log.d("MyMapsApp","onSearch:Exception getLastKnownLocation");
+            Toast.makeText(this, "onSearch: Exception getLastKnowLocation",Toast.LENGTH_SHORT);
+    }
+    if(!location.matches("")){
+            Log.d("MyMapsApp","onSearch: location field is populated");
+            Geocoder geocoder = new Geocoder(this, Locale.US);
+
+            try{
+                addressList = geocoder.getFromLocationName(location,100,
+                        userLocation.latitude - (5.0/60),
+                        userLocation.longitude - (5.0/60),
+                        userLocation.latitude + (5.0/60),
+                        userLocation.longitude + (5.0/60));
+                Log.d("MyMapsApp","onSearch: location field is created");
+
+            }
+            catch (IOException e){
+                e.printStackTrace();
+        }
+        if (!addressList.isEmpty()){
+            Log.d("MyMapsApp","onSearch: AddressList size is: " + addressList.size());
+            for(int i = 0; i<addressList.size();i++){
+                Address address = addressList.get(i);
+                LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+
+                mMap.addMarker(new MarkerOptions().position(latLng).title(i+ ": " + address.getSubThoroughfare()+address.getSubThoroughfare()));
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            }
+
+        }
+    }
     }
 }
